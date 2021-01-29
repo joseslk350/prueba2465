163ebedfcb9a0ea354c3808efa60bcf7df1a88ea
jose <joseyo1976@hotmail.com>
2021-01-29 19:42:30 +0100

    tercercomit

--- app/src/main/java/com/example/prueba2465/MainActivity.kt
+++ app/src/main/java/com/example/prueba2465/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     val boton1 = findViewById<View>(R.id.boton1) as Button
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
